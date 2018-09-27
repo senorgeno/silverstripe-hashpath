3c52f60c617479c8a90b42bf27a9b26d423c056a
Senorgeno <genedower@gmail.com>
2018-09-28 11:19:11 +1200

    Refactor Object to SS_Object

--- _config.php
+++ _config.php
@@ -1,9 +1,13 @@
 <?php
 
+// Ensure compatibility with PHP 7.2 ("object" is a reserved word),
+// with SilverStripe 3.6 (using Object) and SilverStripe 3.7 (using SS_Object)
+if (!class_exists('SS_Object')) class_alias('Object', 'SS_Object');
+
 if (file_exists(__DIR__ . '../vendor/autoload.php')) {
     require_once __DIR__ . '../vendor/autoload.php';
 }
 
 if (class_exists('ContentController')) {
-    Object::add_extension('ContentController', 'HashPathExtension');
+    SS_Object::add_extension('ContentController', 'HashPathExtension');
 }
